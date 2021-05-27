1f4f04dee5835e05dddf94b133f4792878cd01a5
rafaelalma <rafaelalma92@gmail.com>
2021-05-27 19:56:45 +0200

    Game States

---
+++ Assets/Scripts/Game/Controllers/GameStateController.cs
@@ -0,0 +1,25 @@
+﻿public class GameStateController : StateController
+{
+    private void Awake()
+    {
+        IState activeGameState = new ActiveGameState(
+            this
+            );
+        states.Add(typeof(ActiveGameState), activeGameState);
+    }
+
+    private void Start()
+    {
+        SwitchState<ActiveGameState>();
+    }
+
+    private void Update()
+    {
+        currentState.Update();
+    }
+
+    private void FixedUpdate()
+    {
+        currentState.FixedUpdate();
+    }
+}
---
+++ Assets/Scripts/Game/Round/States/EndRoundState.cs
@@ -0,0 +1,29 @@
+﻿public class EndRoundState : State
+{
+    public EndRoundState(
+        IStateController controller
+        ) : base(controller)
+    {
+
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        return;
+    }
+
+    public override void FixedUpdate()
+    {
+        return;
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
